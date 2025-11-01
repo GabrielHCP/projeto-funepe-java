03edb837ec1143b476f3a23a492d82fb739d88ae
GabrielHCP <gabrielcanal360@gmail.com>
2025-11-01 13:42:52 -0300

    Iniciando o projeto

---
+++ src/main/java/com/funepe/menu_digital/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package com.funepe.menu_digital.infrastructure.repository;
+
+import com.funepe.menu_digital.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+
+}
---
+++ src/test/java/com/funepe/menu_digital/MenuDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funepe.menu_digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
